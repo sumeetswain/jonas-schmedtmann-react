617da4a1931978f152d4809559cf562cdcd4d52e
sumeetswain <sumeetswain.work@gmail.com>
2024-12-09 13:01:17 +0530

    added handleUndo, handleUndoLater and Superlike

--- 05-how-react-works/starter/src/App.js
+++ 05-how-react-works/starter/src/App.js
@@ -26,6 +26,8 @@ export default function App() {
   );
 }
 
+
+
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
@@ -39,7 +41,7 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent item={content.at(activeTab)} key={content.at(activeTab).summary} />
       ) : (
         <DifferentContent />
       )}
@@ -63,7 +65,19 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes(likes => likes + 1);
+  }
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+  }
+  function handleTripleInc() {
+    setLikes(likes => likes + 1);
+    setLikes(likes => likes + 1);
+    setLikes(likes => likes + 1);
+  }
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000)
   }
 
   return (
@@ -79,13 +93,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
