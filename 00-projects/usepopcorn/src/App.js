b8d0ad8529d04684e5dc43f8809a89751cf283ec
sumeetswain <sumeetswain.work@gmail.com>
2024-12-12 12:59:26 +0530

    implemented close movie using escape key

--- 00-projects/usepopcorn/src/App.js
+++ 00-projects/usepopcorn/src/App.js
@@ -1,51 +1,5 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating"
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -53,7 +7,7 @@ const average = (arr) =>
 const KEY = "f433faac"
 
 export default function App() {
-  const [query, setQuery] = useState("jab");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
@@ -76,10 +30,10 @@ export default function App() {
     setWatched(watched => watched.filter((movie) => movie.imdbID !== id))
   }
 
+
   useEffect(function () {
 
     const controller = new AbortController()
-
 
     async function fetchMovies() {
       try {
@@ -113,6 +67,7 @@ export default function App() {
       SetError("")
       return
     }
+    handleCloseMovie()
     fetchMovies()
     return function () {
       controller.abort()
@@ -278,6 +233,22 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie)
     onCloseMovie()
   }
+
+  useEffect(function () {
+    function callback(e) {
+      if (e.code === "Escape")
+        onCloseMovie()
+
+    }
+
+    document.addEventListener("keydown", callback)
+
+    return function () {
+      document.removeEventListener("keydown", callback)
+    }
+  }
+    , [onCloseMovie])
+
   useEffect(function () {
     async function getMovieDetails() {
       setIsLoading(true)
