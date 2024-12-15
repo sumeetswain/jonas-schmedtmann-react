ed66ff16e7aad1ed33bf973851e615340abf24cc
sumeetswain <sumeetswain.work@gmail.com>
2024-12-15 13:11:06 +0530

    storing watched list to localStorage using useEffect

--- 00-projects/usepopcorn/src/App-v2.js
+++ 00-projects/usepopcorn/src/App-v2.js
@@ -9,15 +9,11 @@ const KEY = "f433faac"
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
   const [error, SetError] = useState("")
   const [selectedId, setSelectedId] = useState(null)
 
-  // const [watched, setWatched] = useState([]);
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched")
-    return JSON.parse(storedValue)
-  });
   function handleSelectMovie(id) {
     setSelectedId(selectedId => id === selectedId ? null : id)
   }
@@ -28,20 +24,13 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie])
-
-    // localStorage.setItem("watched", JSON.stringify([...watched, movie]))
   }
 
   function handleDeleteWatched(id) {
     setWatched(watched => watched.filter((movie) => movie.imdbID !== id))
   }
 
-  useEffect(function () {
 
-    localStorage.setItem("watched", JSON.stringify(watched))
-
-  },
-    [watched])
 
   useEffect(function () {
 
@@ -314,7 +303,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
           </section>
         </>
       }
-
+      {selectedId}
     </div>)
 }
 
