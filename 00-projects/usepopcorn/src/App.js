0ba5e70222fd2d4688cf1eed4f502e297dfad794
sumeetswain <sumeetswain.work@gmail.com>
2024-12-16 12:19:01 +0530

    created useLocalStorageState to set watched list

--- 00-projects/usepopcorn/src/App.js
+++ 00-projects/usepopcorn/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating"
 import { useMovies } from "./useMovies";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -14,10 +15,12 @@ export default function App() {
 
   const { movies, isLoading, error } = useMovies(query, handleCloseMovie)
 
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched")
-    return JSON.parse(storedValue)
-  });
+  const [watched, setWatched] = useLocalStorageState([], "watched")
+
+  // const [watched, setWatched] = useState(function () {
+  //   const storedValue = localStorage.getItem("watched")
+  //   return JSON.parse(storedValue)
+  // });
   function handleSelectMovie(id) {
     setSelectedId(selectedId => id === selectedId ? null : id)
   }
@@ -36,12 +39,7 @@ export default function App() {
     setWatched(watched => watched.filter((movie) => movie.imdbID !== id))
   }
 
-  useEffect(function () {
 
-    localStorage.setItem("watched", JSON.stringify(watched))
-
-  },
-    [watched])
 
 
 
