cdd3ec665c4478ba15d9f8349265d5130cc1e25f
sumeetswain <sumeetswain.work@gmail.com>
2024-12-24 17:43:18 +0530

    setting up the pages

--- 00-projects/worldwise/src/App.jsx
+++ 00-projects/worldwise/src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
