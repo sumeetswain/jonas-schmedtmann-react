11ed5aa5d6198fd57cc14ccd6c93c8e79336782d
sumeetswain <sumeetswain.work@gmail.com>
2024-12-30 20:28:22 +0530

    refactor: implement CitiesContext for city data management

---
+++ 00-projects/worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const CitiesContext = createContext();
+const URL = "http://localhost:8000";
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (!context) {
+    throw new Error("useCities must be used within a CitiesProvider");
+  }
+  return context;
+}
+export { CitiesProvider, useCities };
