62b9a542eda70d83180845d7a908fe3c8ebc7864
sumeetswain <sumeetswain.work@gmail.com>
2025-01-02 12:10:17 +0530

    feat: integrate CitiesContext into CityItem for active city highlighting

--- 00-projects/worldwise/src/components/CityItem.jsx
+++ 00-projects/worldwise/src/components/CityItem.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
+
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -7,6 +9,7 @@ const formatDate = (date) =>
     year: "numeric",
     weekday: "long",
   }).format(new Date(date));
+
 const flagEmojiToPNG = (flag) => {
   var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
     .map((char) => String.fromCharCode(char - 127397).toLowerCase())
@@ -15,12 +18,16 @@ const flagEmojiToPNG = (flag) => {
     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
   );
 };
+
 export default function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles[`cityItem--active`] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{flagEmojiToPNG(emoji)}</span>
