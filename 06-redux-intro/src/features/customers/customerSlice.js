3982015641b9441d359f83e3d0e98b5f0d2c6950
sumeetswain <sumeetswain.work@gmail.com>
2025-01-22 12:12:21 +0530

    feat: refactor customer slice to use createSlice for improved state management and simplify action creators

--- 06-redux-intro/src/features/customers/customerSlice.js
+++ 06-redux-intro/src/features/customers/customerSlice.js
@@ -1,41 +1,68 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit"
+const initialState = {
     fullName: "",
     nationalID: "",
     createdAt: ""
 }
 // * Reducer Function
 
-export default function customerReducer(currentState = initialStateCustomer, action) {
-    switch (action.type) {
-        case "customer/createCustomer": return {
-            ...currentState,
-            fullName: action.payload.fullName,
-            nationalID: action.payload.nationalID,
-            createdAt: action.payload.createdAt
+const customerSlice = createSlice({
+    name: "customer",
+    initialState,
+    reducers: {
+        createCustomer: {
+            prepare(fullName, nationalID,) {
+                return {
+                    payload: {
+                        fullName,
+                        nationalID,
+                        createdAt: new Date().toISOString()
+                    }
+                }
+            }
+            , reducer(currentState, action) {
+                currentState.fullName = action.payload.fullName
+                currentState.nationalID = action.payload.nationalID
+                currentState.createdAt = action.payload.createdAt
+            },
+        }, updateName(currentState, action) {
+            currentState.fullName = action.payload
         }
-        case "customer/updateName": return {
-            ...currentState,
-            fullName: action.payload
-        }
-        default:
-            return currentState
-
     }
-}
+})
+export const { createCustomer, updateName } = customerSlice.actions
+export default customerSlice.reducer
+// export default function customerReducer(currentState = initialStateCustomer, action) {
+//     switch (action.type) {
+//         case "customer/createCustomer": return {
+//             ...currentState,
+//             fullName: action.payload.fullName,
+//             nationalID: action.payload.nationalID,
+//             createdAt: action.payload.createdAt
+//         }
+//         case "customer/updateName": return {
+//             ...currentState,
+//             fullName: action.payload
+//         }
+//         default:
+//             return currentState
+
+//     }
+// }
 
 // ? Action Creator
-export function createCustomer(fullName, nationalID) {
-    return {
-        type: "customer/createCustomer", payload: {
-            fullName,
-            nationalID,
-            createdAt: new Date().toISOString()
-        }
-    }
-}
-export function updateName(fullName) {
-    return { type: 'customer/updateName', payload: fullName }
-}
+// export function createCustomer(fullName, nationalID) {
+//     return {
+//         type: "customer/createCustomer", payload: {
+//             fullName,
+//             nationalID,
+//             createdAt: new Date().toISOString()
+//         }
+//     }
+// }
+// export function updateName(fullName) {
+//     return { type: 'customer/updateName', payload: fullName }
+// }
 // ! Dispatch Functions
 // store.dispatch(createCustomer("Sumeet Swain", crypto.randomUUID()))
 // console.log(store.getState())
