72985abb4b38546e8208b7abe7da45dacbdda309
sumeetswain <sumeetswain.work@gmail.com>
2025-02-24 13:55:26 +0530

    feat: add order search functionality, implement order loader, and enhance error handling

--- 00-projects/fast-react-pizza/src/ui/AppLayout.jsx
+++ 00-projects/fast-react-pizza/src/ui/AppLayout.jsx
@@ -6,7 +6,7 @@ import Loader from "./Loader";
 export default function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
-  console.log(navigation);
+
   return (
     <div className="layout">
       {isLoading && <Loader />}
