51c04acf0de5e4d16f033121cba1f104fed93ffb
sumeetswain <sumeetswain.work@gmail.com>
2025-02-28 13:51:17 +0530

    feat: enhance UI components with responsive design and improved layout

--- 00-projects/fast-react-pizza/src/features/user/Username.jsx
+++ 00-projects/fast-react-pizza/src/features/user/Username.jsx
@@ -1,3 +1,3 @@
 export default function Username() {
-  return <div className="hidden text-sm font-semibold">Sumeet</div>;
+  return <div className="hidden text-sm font-semibold md:block">Sumeet</div>;
 }
